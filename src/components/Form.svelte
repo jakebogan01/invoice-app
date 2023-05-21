a64df110e647b4cb3e0aa9a11609ebc95bd8ef4b
Jake Bogan <jakebogan01@gmail.com>
2023-05-20 21:31:51 -0400

    added date picker in form component

--- src/components/Form.svelte
+++ src/components/Form.svelte
@@ -1,3 +1,18 @@
+<script>
+     import { DateInput } from 'date-picker-svelte'
+     import { afterUpdate } from 'svelte';
+
+     
+     // const randomDate = new Date(timestamp);
+     // return randomDate.toDateString().split(" ").slice(1).join(" ");
+     
+     let date = new Date();
+
+     afterUpdate(() => {
+          // console.log(date.toDateString().split(" ").slice(1).join(" "));
+     })
+</script>
+
 <form class="border-2 border-red-500">
      <h1 class="text-xl font-semibold leading-7 text-gray-900 pb-10">New Invoice</h1>
      <div class="space-y-12">
@@ -79,6 +94,14 @@
                          </div>
                     </div>
                </div>
+
+               <div class="relative max-w-sm">
+                    <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+                      <svg aria-hidden="true" class="w-5 h-5 text-gray-500" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z" clip-rule="evenodd"></path></svg>
+                    </div>
+                    <DateInput bind:value={date} format="MM-dd-yyyy" />
+               </div>
+               
           </div>
      </div>
    
@@ -87,4 +110,3 @@
           <button type="submit" class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Save</button>
      </div>
 </form>
-   
